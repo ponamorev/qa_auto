00527fe4694de1ece36de3935269a36f7a071bdf
Ponamorev <penzayk@mail.ru>
2020-03-02 23:11:45 +0300

    Added junit 5 examples with several new annotations

---
+++ src/main/java/org/example/ui_maven/drivers/Calculator.java
@@ -0,0 +1,8 @@
+package org.example.ui_maven.drivers;
+
+public class Calculator {
+
+    public int calculate(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/org/example/ui_maven/CalculatorTest.java
@@ -0,0 +1,32 @@
+package org.example.ui_maven;
+
+import org.example.ui_maven.drivers.Calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void addTwoNumber() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.calculate(1, 1), "1 + 1 should equal 2");
+    }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,    1",
+            "1,    2,    3",
+            "49,  51,  100",
+            "1,  100,  101"
+    })
+    void add(int first, int second, int expectedResult) {
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.calculate(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+}
