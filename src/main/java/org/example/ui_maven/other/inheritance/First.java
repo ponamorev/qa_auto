ce3dcfe33d5e87ecb27a8d41cebfcc1e155871fe
Vyacheslav Ponamorev <vy.ponamorev@andersenlab.com>
2020-03-09 11:29:04 +0300

    Add test clases for checking constructor call thread

---
+++ src/main/java/org/example/ui_maven/other/inheritance/First.java
@@ -0,0 +1,10 @@
+package org.example.ui_maven.other.inheritance;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class First {
+    First() {
+        log.info("This is constructor from class First.java");
+    }
+}
---
+++ src/main/java/org/example/ui_maven/other/inheritance/MainCheck.java
@@ -0,0 +1,7 @@
+package org.example.ui_maven.other.inheritance;
+
+public class MainCheck {
+    public static void main(String[] args) {
+        new Third();
+    }
+}
