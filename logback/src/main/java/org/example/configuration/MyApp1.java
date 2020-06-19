1b944c54831c675bdb1609ef6466100c77d4b669
Ponamorev <penzayk@mail.ru>
2020-06-20 00:16:15 +0300

    Added example classes for checking logback configuration

---
+++ logback/src/main/java/org/example/configuration/MyApp1.java
@@ -0,0 +1,16 @@
+package org.example.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyApp1 {
+    final static Logger logger = LoggerFactory.getLogger(MyApp1.class);
+
+    public static void main(String[] args) {
+        logger.info("Entering application.");
+
+        Foo foo = new Foo();
+        foo.doIt();
+        logger.info("Exiting application");
+    }
+}
