da72735a6e7a03f57a785d6cfc953b959af9586d
Ponamorev <penzayk@mail.ru>
2020-10-26 14:06:41 +0300

    Moved allure plugin to assessment_ui module, removed unused import

--- assessment_ui/src/test/java/org/example/ui/LoginYandexTest.java
+++ assessment_ui/src/test/java/org/example/ui/LoginYandexTest.java
@@ -1,16 +1,10 @@
 package org.example.ui;
 
-import org.example.ui.pages.LoginPage;
-import org.example.ui.pages.MailPage;
-import org.example.ui.pages.MainPage;
 import org.example.ui.steps.LoginPageSteps;
 import org.example.ui.steps.MailPageSteps;
 import org.example.ui.steps.MainPageSteps;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Set;
 
 public class LoginYandexTest extends BaseYandexTest {
     private MainPageSteps mainSteps;
